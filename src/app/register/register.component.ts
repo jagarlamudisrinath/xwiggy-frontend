e4d3b47cf460e2a40f0e61ce41608b0794b8cc25
jagarlamudisrinath <jagarlamudisrinath99@gmail.com>
2021-03-05 21:01:25 +0530

    fixing build issues

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AppComponent, User} from "../app.component";
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
-import { environment } from '../environments/environment';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-register',
